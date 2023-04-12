67bee6fc6e111d001c7593c3009e187d5eb68500
“karley888” <1624522123@qq.com>
2023-04-12 19:30:37 +0800

    Site updated: 2023-04-12 19:30:37

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/04/12/我是吊毛/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/04/12/这个是我的第一篇文章/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
